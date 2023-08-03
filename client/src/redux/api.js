5696d6e683699727e179163ce699cbf10957d17a
Harmeet Singh <harmeetchahal9891@gmail.com>
2023-08-03 21:50:23 +0530

    api changes

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
+
 export const getTasks = () => axios.get(`${BASE_URL}/get`);
 export const createTask = (task) => axios.post(`${BASE_URL}/create`, task);
 export const updateTask = (id, task) => axios.patch(`${BASE_URL}/update/${id}`, task);
